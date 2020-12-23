9600f59c6ba75ec4ea1bce548e52423815a6015c
Alex Imsha <alex@imsha.ru>
2020-12-23 17:35:47 +1000

    Сделал каркас TaxPayerClient

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    $client = new \App\Services\Nalog\TaxPayerClient();
+
+    $date = new DateTime();
+
+    return $client->request(406379298934, $date);
+
+
     return view('welcome');
 });
