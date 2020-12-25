ab6705ac6fe347aee0a8d7b3cd257ec1812e2bf8
Alex Imsha <alex@imsha.ru>
2020-12-25 19:03:12 +1000

    Докер

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Landing Page - Start Bootstrap Theme</title>
+    <title>Тестовая работа</title>
     <link href="{{asset('/css/app.css')}}" rel="stylesheet">
 </head>
 
